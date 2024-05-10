01b529891f642d0ab137298d722e32f3101c499d
Shivendra999 <75971495+Shivendra999@users.noreply.github.com>
2024-05-10 18:51:01 -0400

    Delete HW14 directory

--- HW14/src/main/umbcs680/hw14/CustomComparator.java
+++
@@ -1,9 +0,0 @@
-package umbcs680.hw14;
-// CustomComparator.java
-import java.util.Comparator;
-
-public class CustomComparator {
-    public static Comparator<FSElement> nameComparator() {
-        return Comparator.comparing(FSElement::getName, String.CASE_INSENSITIVE_ORDER);
-    }
-}
--- HW14/src/main/umbcs680/hw14/DateSorting.java
+++
@@ -1,9 +0,0 @@
-package umbcs680.hw14;
-// DateSorting.java
-import java.util.Comparator;
-
-public class DateSorting {
-    public static Comparator<FSElement> dateComparator() {
-        return Comparator.comparing(FSElement::getCreationTime);
-    }
-}
